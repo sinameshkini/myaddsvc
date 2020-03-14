d39d4f027d1fcc6701feb3f1d921b499b155f9f1
Sina Meshkini <sinameshkini7@gmail.com>
2020-03-14 16:58:26 +0330

    fixed the problem

--- cmd/addsvc/addsvc.go
+++ cmd/addsvc/addsvc.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	kitgrpc "github.com/go-kit/kit/transport/grpc"
-	"github.com/labstack/gommon/log"
+	"fmt"
+	"log"
+	"net"
+
 	"google.golang.org/grpc"
 	"myaddsvc/pb"
 	"myaddsvc/pkg/addendpoint"
 	"myaddsvc/pkg/addservice"
 	"myaddsvc/pkg/addtransport"
-	"net"
-	"os"
 )
 
 func main()  {
@@ -18,15 +18,33 @@ func main()  {
 		endpoints = addendpoint.New(service)
 		grpcServer = addtransport.NewGRPCServer(endpoints)
 	)
+	_ = grpcServer
+	fmt.Println("Server is running...")
 
-	grpcListener, err := net.Listen("tcp", ":8000")
-	if err != nil{
-		log.Fatalf("gRPC listen error: %v", err)
-		os.Exit(1)
+	// Make a listener
+	lis, err := net.Listen("tcp", "0.0.0.0:8001")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
 	}
-	log.Fatalf("gRPC listening on port :8000")
-	baseServer := grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
-	pb.RegisterAddServer(baseServer, grpcServer)
-	baseServer.Serve(grpcListener)
+
+	// Make a gRPC server
+	grpcS := grpc.NewServer()
+	pb.RegisterAddServer(grpcS, grpcServer)
+
+	// Run the gRPC server
+	if err := grpcS.Serve(lis); err != nil{
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+	//************ Old method *******************
+	//grpcListener, err := net.Listen("tcp", ":8000")
+	//if err != nil{
+	//	log.Fatalf("gRPC listen error: %v", err)
+	//	os.Exit(1)
+	//}
+	//log.Fatalf("gRPC listening on port :8000")
+	//baseServer := grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
+	//pb.RegisterAddServer(baseServer, grpcServer)
+	//baseServer.Serve(grpcListener)
 
 }
